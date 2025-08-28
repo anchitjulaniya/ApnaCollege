a796c0d61c4a230896d56b798108e93b61799973
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2025-08-28 23:49:45 +0530

    resolving cors-2

--- app.js
+++ app.js
@@ -15,18 +15,19 @@ const allowedOrigins = [
 ];
 
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+app.use(cors({ origin: "*", credentials: true }));
 
 
 app.use(express.json());
